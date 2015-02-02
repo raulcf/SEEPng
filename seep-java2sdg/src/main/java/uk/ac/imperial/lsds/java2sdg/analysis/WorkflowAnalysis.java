2d2d4beebd18dddbf370196ef72677cd1325c3f5
raulcf <raul@contxt.in>
2015-02-02 12:59:31 +0000

    ongoing work on workflowanalysis

--- seep-java2sdg/src/main/java/uk/ac/imperial/lsds/java2sdg/analysis/WorkflowAnalysis.java
+++ seep-java2sdg/src/main/java/uk/ac/imperial/lsds/java2sdg/analysis/WorkflowAnalysis.java
@@ -1,27 +1,19 @@
 package uk.ac.imperial.lsds.java2sdg.analysis;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.codehaus.commons.compiler.CompileException;
-import org.codehaus.commons.compiler.Location;
 import org.codehaus.janino.Java;
-import org.codehaus.janino.Java.ConstructorDeclarator;
-import org.codehaus.janino.Java.FieldDeclaration;
-import org.codehaus.janino.Java.Initializer;
-import org.codehaus.janino.Java.MemberClassDeclaration;
-import org.codehaus.janino.Java.MemberInterfaceDeclaration;
-import org.codehaus.janino.Java.MethodDeclarator;
-import org.codehaus.janino.Java.Scope;
-import org.codehaus.janino.Java.TypeBodyDeclaration;
-import org.codehaus.janino.Java.TypeDeclaration;
-import org.codehaus.janino.Visitor.TypeBodyDeclarationVisitor;
+import org.codehaus.janino.Java.BlockStatement;
+import org.codehaus.janino.Java.ExpressionStatement;
 import org.codehaus.janino.util.Traverser;
 
 import uk.ac.imperial.lsds.java2sdg.bricks.WorkflowRepr;
 
 public class WorkflowAnalysis extends Traverser {
 
-	private Map<String, WorkflowRepr> workflows;
+	private Map<String, WorkflowRepr> workflows = new HashMap<>();
 	
 	public static Map<String, WorkflowRepr> getWorkflows(Java.CompilationUnit cu){
 		WorkflowAnalysis wa = new WorkflowAnalysis();
@@ -32,9 +24,19 @@ public class WorkflowAnalysis extends Traverser {
 	@Override
 	public void traverseMethodDeclarator(Java.MethodDeclarator md){
 		if(md.name.equals("main")){
-			
+			List<? extends BlockStatement> statements = md.optionalStatements;
+			for(BlockStatement stmt : statements){
+				if(stmt instanceof Java.ExpressionStatement){
+					Java.ExpressionStatement expr = (Java.ExpressionStatement)stmt;
+					System.out.println("Expr: "+expr.toString());
+					System.out.println("RVALUE: "+expr.rvalue.toString());
+				}
+//				System.out.println("SCOPE: "+stmt.getEnclosingScope().toString());
+//				System.out.println("STMT: "+stmt.toString());
+//				System.out.println("class: "+stmt.getClass().getSimpleName());
+			}
 			
 		}
 	}
-	
+
 }
