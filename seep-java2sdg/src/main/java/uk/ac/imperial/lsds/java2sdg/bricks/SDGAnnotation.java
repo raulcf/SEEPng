828230ae88ab9f25979a23212efeb1b7fe3ca6d8
pgaref <pgaref@macbook-air.lan>
2015-02-08 22:12:31 +0000

    SDG enum extra functionality to automate annotation parsing in AnalysisUtils

--- seep-java2sdg/src/main/java/uk/ac/imperial/lsds/java2sdg/bricks/SDGAnnotation.java
+++ seep-java2sdg/src/main/java/uk/ac/imperial/lsds/java2sdg/bricks/SDGAnnotation.java
@@ -11,10 +11,42 @@
 package uk.ac.imperial.lsds.java2sdg.bricks;
 
 public enum SDGAnnotation{
-	PARTITIONED, 
-	PARTIAL_STATE, 
-	PARTIAL_DATA, 
-	GLOBAL,
+	PARTITIONED ("Partitioned"), 
+	PARTIAL_STATE ("PartialState"), 
+	PARTIAL_DATA ("PartialData"), 
+	GLOBAL ("Global"),
+	GLOBAL_WRITE ("GlobalWrite"),
+	GLOBAL_READ ("GlobalRead"),
+	COLLECTION ("Collection"),
+	LOCAL ("Local"),
+	FILE ("File"),
+	NETWORKSOURCE ("NetworkSource"),
+	CONSOLESINK ("ConsoleSink");
 	
-	GLOBAL_WRITE, GLOBAL_READ, COLLECTION, LOCAL, FILE, NETWORKSOURCE, CONSOLESINK
+	
+	private final String value;
+	
+	private SDGAnnotation(String v) {
+		value = v;
+	}
+	
+	public String getValue(){
+		return value;
+	}
+	/*
+	 * Method to automatically identify  annotation from its representation
+	 * Partial seems to be the same case as Partial_State so there is an extra condition
+	 * This methods throws an IllegalArgumentException if the value is not an acceptable Annotation  
+	 */
+	public static SDGAnnotation getAnnotation(String value) {
+		
+		if(value.equalsIgnoreCase("Partial"))
+			return SDGAnnotation.PARTIAL_STATE;
+		
+        for(SDGAnnotation v : values())
+            if(v.getValue().equalsIgnoreCase(value)) 
+            	return v;
+        throw new IllegalArgumentException();
+    }
+	
 }
