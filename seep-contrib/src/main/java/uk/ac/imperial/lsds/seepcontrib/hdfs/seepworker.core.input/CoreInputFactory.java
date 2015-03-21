5d82d2d89587d3f4bae3c5118a1d9a1bff07c566
David Chia <david.mhchia@gmail.com>
2015-03-21 13:46:28 +0000

    Added hdfs files to hdfs-integration branch

---
+++ seep-contrib/src/main/java/uk/ac/imperial/lsds/seepcontrib/hdfs/seepworker.core.input/CoreInputFactory.java
@@ -0,0 +1,79 @@
+package uk.ac.imperial.lsds.seepworker.core.input;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.imperial.lsds.seep.api.ConnectionType;
+import uk.ac.imperial.lsds.seep.api.DataStoreType;
+import uk.ac.imperial.lsds.seep.api.PhysicalOperator;
+import uk.ac.imperial.lsds.seep.api.UpstreamConnection;
+import uk.ac.imperial.lsds.seep.core.InputAdapter;
+import uk.ac.imperial.lsds.seepworker.WorkerConfig;
+
+public class CoreInputFactory {
+
+	final private static Logger LOG = LoggerFactory.getLogger(CoreInputFactory.class);
+	
+	public static CoreInput buildCoreInputForOperator(WorkerConfig wc, PhysicalOperator o){
+		LOG.info("Building Core Input...");
+		List<InputAdapter> inputAdapters = new LinkedList<InputAdapter>();
+		// Create an InputAdapter per upstream connection -> know with the streamId
+		Map<Integer, List<UpstreamConnection>> streamToOpConn = new HashMap<Integer, List<UpstreamConnection>>();
+		for(UpstreamConnection uc : o.upstreamConnections()){
+			int streamId = uc.getStreamId();
+			if(streamToOpConn.containsKey(streamId)){
+				streamToOpConn.get(streamId).add(uc);
+			}
+			else{
+				List<UpstreamConnection> l = new ArrayList<UpstreamConnection>();
+				l.add(uc);
+				streamToOpConn.put(streamId, l);
+			}
+		}
+		// Perform sanity check. All ops for a given streamId should have the same connType
+		for(List<UpstreamConnection> l : streamToOpConn.values()){
+			ConnectionType ct = null;
+			for(UpstreamConnection oct : l){
+				if(ct == null){
+					ct = oct.getConnectionType();
+				}
+				if(!ct.equals(oct.getConnectionType())){
+					// TODO: throw error
+					System.out.println("Sanity check FAILED");
+					System.exit(0);
+				}
+			}
+		}
+		// Build an input adapter per opId grouped by streamId
+		for(Integer streamId : streamToOpConn.keySet()){
+			List<InputAdapter> ias = null;
+			List<UpstreamConnection> upCon = streamToOpConn.get(streamId);
+			DataStoreType dOriginType = upCon.get(0).getDataOriginType();
+			if(dOriginType.equals(DataStoreType.NETWORK)){
+				ias = InputAdapterFactory.buildInputAdapterOfTypeNetworkForOps(wc, streamId, upCon);
+			} 
+			else if(dOriginType.equals(DataStoreType.FILE)){
+				ias = InputAdapterFactory.buildInputAdapterOfTypeFileForOps(wc, streamId, upCon);
+			}
+			else if(dOriginType.equals(DataStoreType.KAFKA)){
+				ias = InputAdapterFactory.buildInputAdapterOfTypeKafkaForOps(wc, streamId, upCon);
+			}
+			else if(dOriginType.equals(DataStoreType.HDFS)){
+				ias = InputAdapterFactory.buildInputAdapterOfTypeHdfsForOps(wc, streamId, upCon);
+			}
+			if(ias != null){
+				inputAdapters.addAll(ias);
+			}
+		}
+		CoreInput cInput = new CoreInput(inputAdapters);
+		LOG.info("Building Core Input...OK");
+		return cInput;
+	}
+	
+}
---
+++ seep-contrib/src/main/java/uk/ac/imperial/lsds/seepcontrib/hdfs/seepworker.core.input/InputAdapterFactory.java
@@ -0,0 +1,105 @@
+package uk.ac.imperial.lsds.seepworker.core.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.imperial.lsds.seep.api.ConnectionType;
+import uk.ac.imperial.lsds.seep.api.UpstreamConnection;
+import uk.ac.imperial.lsds.seep.api.data.Schema;
+import uk.ac.imperial.lsds.seep.comm.IOComm;
+import uk.ac.imperial.lsds.seep.core.InputAdapter;
+import uk.ac.imperial.lsds.seepcontrib.hdfs.comm.HdfsDataStream;
+import uk.ac.imperial.lsds.seepcontrib.kafka.comm.KafkaDataStream;
+import uk.ac.imperial.lsds.seepworker.WorkerConfig;
+
+public class InputAdapterFactory {
+
+	final static private Logger LOG = LoggerFactory.getLogger(IOComm.class.getName());
+	
+	public static List<InputAdapter> buildInputAdapterOfTypeNetworkForOps(WorkerConfig wc, int streamId, List<UpstreamConnection> upc){
+		List<InputAdapter> ias = new ArrayList<InputAdapter>();
+		short cType = upc.get(0).getConnectionType().ofType();
+		Schema expectedSchema = upc.get(0).getExpectedSchema();
+		
+		if(cType == ConnectionType.ONE_AT_A_TIME.ofType()){
+			// one-queue-per-conn, one-single-queue, etc.
+			LOG.info("Creating inputAdapter for upstream streamId: {} of type {}", streamId, "ONE_AT_A_TIME");
+			InputAdapter ia = null;
+			for(UpstreamConnection uc : upc){
+				int opId = uc.getUpstreamOperator().getOperatorId();
+				ia = new NetworkDataStream(wc, opId, streamId, expectedSchema);
+				ias.add(ia);
+			}
+		}
+		else if(cType == ConnectionType.UPSTREAM_SYNC_BARRIER.ofType()){
+			// one barrier for all connections within the same barrier
+			LOG.info("Creating NETWORK inputAdapter for upstream streamId: {} of type {}", streamId, ConnectionType.UPSTREAM_SYNC_BARRIER.withName());
+			InputAdapter ia = new NetworkBarrier(wc, streamId, expectedSchema, upc);
+			ias.add(ia);
+		}
+		else if(cType == ConnectionType.BATCH.ofType()){
+			
+		}
+		else if(cType == ConnectionType.ORDERED.ofType()){
+			
+		}
+		else if(cType == ConnectionType.WINDOW.ofType()){
+			
+		}
+		return ias;
+	}
+	
+	public static List<InputAdapter> buildInputAdapterOfTypeFileForOps(WorkerConfig wc, int streamId, List<UpstreamConnection> upc){
+		List<InputAdapter> ias = new ArrayList<InputAdapter>();
+		short cType = upc.get(0).getConnectionType().ofType();
+		Schema expectedSchema = upc.get(0).getExpectedSchema();
+		
+		if(cType == ConnectionType.ONE_AT_A_TIME.ofType()){
+			LOG.info("Creating FILE inputAdapter for upstream streamId: {} of type {}", streamId, ConnectionType.ONE_AT_A_TIME.withName());
+			for(UpstreamConnection uc : upc){
+				int opId = uc.getUpstreamOperator().getOperatorId();
+				InputAdapter ia = new FileDataStream(wc, opId, streamId, expectedSchema);
+				ias.add(ia);
+			}
+		}
+		else if(cType == ConnectionType.BATCH.ofType()){
+			
+		}
+		
+		return ias;
+	}
+	
+	public static List<InputAdapter> buildInputAdapterOfTypeKafkaForOps(WorkerConfig wc, int streamId, List<UpstreamConnection> upc){
+		List<InputAdapter> ias = new ArrayList<InputAdapter>();
+		short cType = upc.get(0).getConnectionType().ofType();
+		Schema expectedSchema = upc.get(0).getExpectedSchema();
+		if(cType == ConnectionType.ONE_AT_A_TIME.ofType()){
+			LOG.info("Creating KAFKA inputAdapter for upstream streamId: {} of type {}", streamId, ConnectionType.ONE_AT_A_TIME.withName());
+			for(UpstreamConnection uc : upc){
+				int opId = uc.getUpstreamOperator().getOperatorId();
+				// FIXME: get worker configs from WorkerConfig and KAFKA-specific configs from (KafkaConfig)uc.getDataOrigin().getConfig()
+				InputAdapter ia = new KafkaDataStream(opId, streamId, expectedSchema);
+				ias.add(ia);
+			}
+		}
+		return ias;
+	}
+	public static List<InputAdapter> buildInputAdapterOfTypeHdfsForOps(WorkerConfig wc, int streamId, List<UpstreamConnection> upc){
+		List<InputAdapter> ias = new ArrayList<InputAdapter>();
+		short cType = upc.get(0).getConnectionType().ofType();
+		Schema expectedSchema = upc.get(0).getExpectedSchema();
+		if(cType == ConnectionType.BATCH.ofType()){
+			LOG.info("Creating HDFS inputAdapter for upstream streamId: {} of type {}", streamId, ConnectionType.BATCH.withName());
+			for(UpstreamConnection uc : upc){
+				int opId = uc.getUpstreamOperator().getOperatorId();
+				InputAdapter ia = new HdfsDataStream(opId,streamId,expectedSchema);
+				ias.add(ia);
+			}
+		}
+		return ias;
+	}
+	
+}
