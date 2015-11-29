e5498f5ef95873284dcd71f2769110362a3e8b4b
Ra <raul@contxt.in>
2015-11-28 22:03:08 -0500

    Added support for longer writes by using buffer pool. Adapter interface to new one. Cleaning code

--- seep-worker/src/main/java/uk/ac/imperial/lsds/seepworker/core/Dataset.java
+++ seep-worker/src/main/java/uk/ac/imperial/lsds/seepworker/core/Dataset.java
@@ -3,7 +3,10 @@ package uk.ac.imperial.lsds.seepworker.core;
 import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 import uk.ac.imperial.lsds.seep.api.DataReference;
 import uk.ac.imperial.lsds.seep.api.data.Schema;
@@ -17,36 +20,64 @@ public class Dataset implements IBuffer, OBuffer {
 	private DataReference dataReference;
 	private BufferPool bufferPool;
 	
-	
-	public ByteBuffer buffer = ByteBuffer.allocate(8192);
-	private final int BATCH_SIZE = 9; // FIXME: rethink this, too much overhead per tuple
-	
-	// Writing artifacts
-	private AtomicBoolean completed = new AtomicBoolean(false);
-	private int tuplesInBatch = 0;
-	private int currentBatchSize = 0;
+	private Queue<ByteBuffer> buffers;
+	private Iterator<ByteBuffer> readerIterator;
+	private ByteBuffer wPtrToBuffer;
+	private ByteBuffer rPtrToBuffer;
 
 	public Dataset(DataReference dataReference, BufferPool bufferPool) {
 		this.dataReference = dataReference;
 		this.id = dataReference.getId();
 		this.bufferPool = bufferPool;
-		allocateInitialBuffer();
+		this.wPtrToBuffer = bufferPool.borrowBuffer();
+		this.wPtrToBuffer.position(TupleInfo.PER_BATCH_OVERHEAD_SIZE);
+		this.buffers = new LinkedList<>();
+		this.buffers.add(wPtrToBuffer);
 	}
 	
 	public Dataset(int id, byte[] syntheticData, DataReference dr) {
 		this.dataReference = dr;
 		this.id = id;
 		// FIXME: for now, just make sure nobody writes and reads this simultaneously
-		this.buffer = ByteBuffer.allocate(8192);
+		this.wPtrToBuffer = bufferPool.borrowBuffer();
 		// This data is ready to be simply copied over
-		buffer.put(syntheticData);
+		wPtrToBuffer.put(syntheticData);
+		this.buffers = new LinkedList<>();
+		this.buffers.add(wPtrToBuffer);
 	}
 	
-	// FIXME: this guy will use the bufferPool to get the buffer. In general no one allocates mem like this ever
-	private void allocateInitialBuffer() {
-		// FIXME: for now, just make sure nobody writes and reads this simultaneously
-		this.buffer = ByteBuffer.allocate(8192);
-		this.buffer.position(TupleInfo.PER_BATCH_OVERHEAD_SIZE);
+	public List<byte[]> consumeData(int numTuples) {
+		// TODO: Implement
+		return null;
+	}
+	
+	public byte[] consumeData() {
+		// Lazily initialize Iterator
+		if(readerIterator == null) {
+			readerIterator = this.buffers.iterator();
+		}
+		
+		// Get next buffer for reading
+		if(rPtrToBuffer == null || rPtrToBuffer.remaining() == 0) {
+			// When the buffer is read completely we return it to the pool
+			if(rPtrToBuffer != null) {
+				bufferPool.returnBuffer(rPtrToBuffer);
+			}
+			if(readerIterator.hasNext()) {
+				rPtrToBuffer = readerIterator.next();
+				rPtrToBuffer.flip();
+			}
+			else {
+				// done reading
+				return null;
+			}
+		}
+		
+		int size = rPtrToBuffer.getInt();
+		byte[] data = new byte[size];
+		rPtrToBuffer.get(data);
+		
+		return data;
 	}
 	
 	public Schema getSchemaForDataset() {
@@ -54,9 +85,51 @@ public class Dataset implements IBuffer, OBuffer {
 	}
 	
 	/**
-	 * IBuffer
+	 * OBuffer
+	 */
+
+	@Override
+	public int id() {
+		return id;
+	}
+
+	@Override
+	public DataReference getDataReference() {
+		return dataReference;
+	}
+
+	@Override
+	public boolean drainTo(WritableByteChannel channel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public boolean write(byte[] data) {
+		
+		int dataSize = data.length;
+		if(wPtrToBuffer.remaining() < dataSize + TupleInfo.TUPLE_SIZE_OVERHEAD) {
+			// Borrow a new buffer and add to the collection
+			this.wPtrToBuffer = bufferPool.borrowBuffer();
+			this.buffers.add(wPtrToBuffer);
+		}
+		
+		wPtrToBuffer.putInt(dataSize);
+		wPtrToBuffer.put(data);
+		
+		return true;
+	}
+	
+	/**
+	 * IBuffer interface
 	 */
 	
+	@Override
+	public boolean readyToWrite() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 	@Override
 	public void readFrom(ReadableByteChannel channel) {
 		// TODO Auto-generated method stub
@@ -73,57 +146,6 @@ public class Dataset implements IBuffer, OBuffer {
 	public void pushData(byte[] data) {
 		// TODO Auto-generated method stub
 		
-	}
-	
-	/**
-	 * OBuffer
-	 */
-
-	@Override
-	public int id() {
-		return id;
-	}
-
-	@Override
-	public DataReference getDataReference() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean drainTo(WritableByteChannel channel) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean write(byte[] data) {
-		
-		// FIXME: assume there is always enough space
-		
-		int tupleSize = data.length;
-		buffer.putInt(tupleSize);
-		buffer.put(data);
-		tuplesInBatch++;
-		currentBatchSize = currentBatchSize + tupleSize + TupleInfo.TUPLE_SIZE_OVERHEAD;
-		
-		if(buffer.position() >= BATCH_SIZE) {
-			int currentPosition = buffer.position();
-			int currentLimit = buffer.limit();
-			buffer.position(TupleInfo.NUM_TUPLES_BATCH_OFFSET);
-			buffer.putInt(tuplesInBatch);
-			buffer.putInt(currentBatchSize);
-			buffer.position(currentPosition);
-			buffer.limit(currentLimit);
-			//buffer.flip(); // leave the buffer ready to be read FIXME: not in store mode....
-		}
-		return true;
-	}
-
-	@Override
-	public boolean readyToWrite() {
-		// TODO Auto-generated method stub
-		return false;
 	}
 	
 }
--- seep-worker/src/main/java/uk/ac/imperial/lsds/seepworker/core/input/DatasetInputAdapter.java
+++ seep-worker/src/main/java/uk/ac/imperial/lsds/seepworker/core/input/DatasetInputAdapter.java
@@ -1,14 +1,10 @@
 package uk.ac.imperial.lsds.seepworker.core.input;
 
-import java.nio.ByteBuffer;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.List;
 
 import uk.ac.imperial.lsds.seep.api.DataStoreType;
 import uk.ac.imperial.lsds.seep.api.data.ITuple;
 import uk.ac.imperial.lsds.seep.api.data.Schema;
-import uk.ac.imperial.lsds.seep.api.data.TupleInfo;
 import uk.ac.imperial.lsds.seep.core.InputAdapter;
 import uk.ac.imperial.lsds.seep.core.InputAdapterReturnType;
 import uk.ac.imperial.lsds.seepworker.WorkerConfig;
@@ -22,18 +18,11 @@ public class DatasetInputAdapter implements InputAdapter {
 	private Dataset dataset;
 	private ITuple iTuple;
 	
-	private ByteBuffer header = ByteBuffer.allocate(TupleInfo.PER_BATCH_OVERHEAD_SIZE);
-	private ByteBuffer payload = null;
-	private int nTuples = 0;
-	
-	private Deque<byte[]> iTupleBuffer;
-	
 	public DatasetInputAdapter(WorkerConfig wc, int streamId, Dataset dataset) {
 		this.streamId = streamId;
 		this.dataset = dataset;
 		Schema expectedSchema = this.dataset.getSchemaForDataset();
 		this.iTuple = new ITuple(expectedSchema);
-		this.iTupleBuffer = new ArrayDeque<>();
 	}
 	
 	@Override
@@ -51,69 +40,18 @@ public class DatasetInputAdapter implements InputAdapter {
 		// In this case it's dynamic.
 		return dataset.getDataReference().getDataStore().type();
 	}
-
+	
 	@Override
 	public ITuple pullDataItem(int timeout) {
-		
-		if(iTupleBuffer.size() == 0) {
-		
-			// FIXME: This buffer is pointing to the last written position. THINK this
-			ByteBuffer sourceData = dataset.buffer;
-			// FIXME: temporal solution?
-			sourceData.flip();
-			// TODO: write a more convenient method, if possible readablebytechannel to 
-			// so basically create a readablebytechannel from sourceData
-			
-			// Read first into a stream from the dataset (should be indep of where dataset keeps )
-			if(header.remaining() > 0) {
-				this.read(sourceData, header);
-			}
-			
-			if(payload == null && !header.hasRemaining()) {
-				header.flip();
-				byte control = header.get();
-				nTuples = header.getInt();
-				int payloadSize = header.getInt(); // payload size
-				payload = ByteBuffer.allocate(payloadSize);
-			}
-			
-			if(payload != null) {
-				this.read(sourceData, payload);
-				if(!payload.hasRemaining()) {
-					
-					int tupleSize = 0;
-					payload.flip(); // Prepare buffer to read
-					for(int i = 0; i < nTuples; i++){			
-						tupleSize = payload.getInt();
-						byte[] completedRead = new byte[tupleSize];
-						payload.get(completedRead, 0, tupleSize);
-						
-						iTupleBuffer.add(completedRead);
-					}
-					payload.clear();
-					payload = null;
-					header.clear();
-					nTuples = 0;
-				}
-			}
-		}
-		byte[] data = iTupleBuffer.poll();
+		byte[] data = dataset.consumeData();
 		iTuple.setData(data);
 		iTuple.setStreamId(streamId);
 		return iTuple;
 	}
-	
-	private int read(ByteBuffer src, ByteBuffer dst) {
-		int offset = src.position();
-		int length = dst.remaining();
-		dst.put(src.array(), offset, length);
-		src.position(offset + length); // advance position after reading from backup array
-		return 0;
-	}
 
 	@Override
 	public List<ITuple> pullDataItems(int timeout) {
-		// TODO: yeah
+		// TODO: will use Dataset.read(int number elements);
 		return null;
 	}
 
