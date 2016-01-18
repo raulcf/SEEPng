f3eab5f6dfae9323d6661bedfa2a4a9b65a2c2a3
Panagiotis Garefalakis <pangaref@gmail.com>
2016-01-18 11:47:43 +0000

    Proper error case hanlding throwing exceptions in Schema class

--- seep-common/src/main/java/uk/ac/imperial/lsds/seep/api/data/Schema.java
+++ seep-common/src/main/java/uk/ac/imperial/lsds/seep/api/data/Schema.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import uk.ac.imperial.lsds.seep.errors.SchemaException;
 import uk.ac.imperial.lsds.seep.util.Utils;
 
 public class Schema {
@@ -131,7 +132,7 @@ public class Schema {
 		public SchemaBuilder newField(Type type, String name){
 			// safety checks
 			if(names.contains(name)){
-				// TODO: throw error
+				throw new SchemaException("Schema already contains a field with name: "+ name);
 			}
 			this.fields.add(type);
 			this.names.add(name);
@@ -141,7 +142,7 @@ public class Schema {
 		public Schema build(){
 			// Sanity check
 			if(! (fields.size() == names.size())){
-				// TODO: throw error
+				throw new SchemaException("Name-Field Missmatch - Each Type should be mapped to exactly one Name");
 			}
 			Type[] f = new Type[fields.size()];
 			f = fields.toArray(f);
