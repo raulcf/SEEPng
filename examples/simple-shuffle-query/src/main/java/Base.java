a3d749eef617e2d9c32e36f66f6e2d498164bd68
Ra <raul@contxt.in>
2016-02-10 22:54:19 -0500

    Modified example so that it can be serialized (classes in independent files)

--- examples/simple-shuffle-query/src/main/java/Base.java
+++ examples/simple-shuffle-query/src/main/java/Base.java
@@ -1,17 +1,13 @@
-import java.util.List;
-
 import uk.ac.imperial.lsds.seep.api.operator.LogicalOperator;
 import uk.ac.imperial.lsds.seep.api.operator.SeepLogicalQuery;
 import uk.ac.imperial.lsds.seep.api.operator.sinks.MarkerSink;
 import uk.ac.imperial.lsds.seep.api.operator.sources.SyntheticSource;
 import uk.ac.imperial.lsds.seep.api.state.SeepState;
-import uk.ac.imperial.lsds.seep.api.API;
+import uk.ac.imperial.lsds.seep.api.state.stateimpl.SeepMap;
 import uk.ac.imperial.lsds.seep.api.DataStore;
 import uk.ac.imperial.lsds.seep.api.DataStoreType;
 import uk.ac.imperial.lsds.seep.api.QueryComposer;
 import uk.ac.imperial.lsds.seep.api.QueryExecutionMode;
-import uk.ac.imperial.lsds.seep.api.SeepTask;
-import uk.ac.imperial.lsds.seep.api.data.ITuple;
 import uk.ac.imperial.lsds.seep.api.data.Schema;
 import uk.ac.imperial.lsds.seep.api.data.Schema.SchemaBuilder;
 import uk.ac.imperial.lsds.seep.api.data.Type;
@@ -27,8 +23,8 @@ public class Base implements QueryComposer {
 		SyntheticSource synSrc = SyntheticSource.newSource(0, null);
 		LogicalOperator map = queryAPI.newStatelessOperator(new Map(), 1);
 		
-		SeepState ss = null;
-		LogicalOperator reduce = queryAPI.newStatefulOperator(new Reduce(), null, 2);
+		SeepState ss = new SeepMap();
+		LogicalOperator reduce = queryAPI.newStatefulOperator(new Reduce(), ss, 2);
 		MarkerSink snk = MarkerSink.newSink(3);
 		
 		synSrc.connectTo(map, schema, 0);
@@ -39,64 +35,6 @@ public class Base implements QueryComposer {
 		slq.setExecutionModeHint(QueryExecutionMode.ALL_SCHEDULED);
 		
 		return slq;
-	}
-	
-	
-	
-	class Map implements SeepTask {
-
-		@Override
-		public void close() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void processData(ITuple arg0, API arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void processDataGroup(List<ITuple> arg0, API arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setUp() {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-	
-	class Reduce implements SeepTask {
-
-		@Override
-		public void close() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void processData(ITuple arg0, API arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void processDataGroup(List<ITuple> arg0, API arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setUp() {
-			// TODO Auto-generated method stub
-			
-		}
-		
 	}
 
 }
---
+++ examples/simple-shuffle-query/src/main/java/Map.java
@@ -0,0 +1,46 @@
+import java.util.List;
+
+import uk.ac.imperial.lsds.seep.api.API;
+import uk.ac.imperial.lsds.seep.api.SeepTask;
+import uk.ac.imperial.lsds.seep.api.data.ITuple;
+import uk.ac.imperial.lsds.seep.api.data.OTuple;
+import uk.ac.imperial.lsds.seep.api.data.Schema;
+import uk.ac.imperial.lsds.seep.api.data.Type;
+import uk.ac.imperial.lsds.seep.api.data.Schema.SchemaBuilder;
+
+
+public class Map implements SeepTask {
+
+	Schema schema = SchemaBuilder.getInstance().newField(Type.INT, "userId").newField(Type.LONG, "value").build();
+	
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processData(ITuple arg0, API api) {
+		int userId = arg0.getInt("userId");
+		long value = arg0.getLong("value");
+		
+		userId++;
+		value++;
+		
+		byte[] ot = OTuple.create(schema, schema.names(), new Object[]{userId, value});
+		api.sendKey(ot, userId);
+	}
+
+	@Override
+	public void processDataGroup(List<ITuple> arg0, API arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUp() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
