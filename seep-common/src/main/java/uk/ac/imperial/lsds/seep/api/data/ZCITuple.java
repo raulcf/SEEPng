ff2ec43fe0c8752d92b21cd98704362767a607b9
vlopezmo <v.lopez-morales@imperial.ac.uk>
2016-06-22 15:21:20 +0100

    Adding types float and double support to the ZCITuple class

--- seep-common/src/main/java/uk/ac/imperial/lsds/seep/api/data/ZCITuple.java
+++ seep-common/src/main/java/uk/ac/imperial/lsds/seep/api/data/ZCITuple.java
@@ -62,5 +62,47 @@ public class ZCITuple extends ITuple {
 		ptr.position(ptrPosition);
 		return ptr.getLong();
 	}
+	
+	public float getFloat(String fieldName){
+		if(! schema.hasField(fieldName)){
+			throw new SchemaException("Current Schema does not have a field with name '"+fieldName+ "'");
+		}
+		if(! schema.typeCheck(fieldName, Type.FLOAT)) {
+			throw new SchemaException("Current Schema cannot typeCheck a field type '"+ Type.FLOAT +"' with name '"+fieldName+"'");
+		}
+		
+		int offset = mapFieldToOffset.get(fieldName);
+		int ptrPosition = bufferPtrPosition + offset;
+		ptr.position(ptrPosition);
+		return ptr.getFloat();
+	}
+	
+	public float getFloat(int idx) {
+		int offset = mapIdxToOffset[idx];
+		int ptrPosition = bufferPtrPosition + offset;
+		ptr.position(ptrPosition);
+		return ptr.getFloat();
+	}
+	
+	public double getDouble(String fieldName){
+		if(! schema.hasField(fieldName)){
+			throw new SchemaException("Current Schema does not have a field with name '"+fieldName+ "'");
+		}
+		if(! schema.typeCheck(fieldName, Type.DOUBLE)) {
+			throw new SchemaException("Current Schema cannot typeCheck a field type '"+ Type.DOUBLE +"' with name '"+fieldName+"'");
+		}
+		
+		int offset = mapFieldToOffset.get(fieldName);
+		int ptrPosition = bufferPtrPosition + offset;
+		ptr.position(ptrPosition);
+		return ptr.getDouble();
+	}
+	
+	public double getDouble(int idx) {
+		int offset = mapIdxToOffset[idx];
+		int ptrPosition = bufferPtrPosition + offset;
+		ptr.position(ptrPosition);
+		return ptr.getDouble();
+	}
  
 }
